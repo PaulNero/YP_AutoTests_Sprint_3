546b1fd48ef9920359fedea364e628401f7bcd93
PaulNero <58290662+PaulNero@users.noreply.github.com>
2025-10-06 22:56:07 +0300

    fix conftest for docker/ci environment

--- conftest.py
+++ conftest.py
@@ -3,6 +3,8 @@ import random
 import string
 from src import links
 from selenium import webdriver
+import tempfile
+import shutil
 
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
@@ -13,10 +15,25 @@ def driver():
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
     options.add_argument('--window-size=1920,1080')
+    options.add_argument('--no-sandbox') # Необходимо для запуска Chrome в Docker/CI окружении
+    options.add_argument('--disable-dev-shm-usage') # Необходимо для запуска Chrome в Docker/CI окружении
+    
+    # Создаём уникальную временную директорию для каждого теста
+    user_data_dir = tempfile.mkdtemp()
+    options.add_argument(f'--user-data-dir={user_data_dir}')
+    
     driver = webdriver.Chrome(options=options)
     driver.get(links.main_link)
+    
     yield driver
+    
     driver.quit()
+    
+    # Удаляем временную директорию
+    try:
+        shutil.rmtree(user_data_dir)
+    except:
+        pass
 
 @pytest.fixture
 def new_email():
