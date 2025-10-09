917503b1bec27352c1d77741b9d2695b25ea1822
PaulNero <58290662+PaulNero@users.noreply.github.com>
2025-10-09 22:03:07 +0300

    big test refactor

---
+++ src/steps.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+import pytest
+import allure
+
+from src import data
+from src import links
+from src import locators
+from src.wait_er import wait_and_click, wait_and_send_keys, wait_for_url_contains
+
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+def click_on_button_private_cabinet(driver):
+    with allure.step('Кликнуть по кнопке "Личный кабинет"'):
+        wait_and_click(driver, locator=locators.button_private_cabinet)
+        
+def click_on_button_enter_to_account_on_main_page(driver):
+    with allure.step('Кликнуть по кнопке "Войти в аккаунт"'):    
+        wait_and_click(driver, locator=locators.button_enter_to_account_on_main_page)
+        
+def click_on_button_restration_on_login_page(driver):
+    with allure.step('Нажать на кнопку "Зарегистрироваться"'): 
+        wait_and_click(driver, locator=locators.button_restration_on_login_page)
+        
+def input_registered_user_name(driver, username):
+    with allure.step('Ввести имя регистрируемого пользователя'):
+        wait_and_send_keys(driver, locator=locators.input_name_for_registration, value=username)
+
+def input_registered_user_email(driver, email):
+    with allure.step('Ввести email регистрируемого пользователя'):
+        wait_and_send_keys(driver, locator=locators.input_email_for_registration, value=email)
+        
+def input_password(driver, password):
+    with allure.step('Ввести пароль регистрируемого пользователя'):
+        wait_and_send_keys(driver, locator=locators.input_password, value=password)
+
+def click_on_button_registration_complete(driver):
+    with allure.step('Кликнуть по кнопке "Зарегистрироваться"'):
+        wait_and_click(driver, locator=locators.button_registration_complite)
+
+def check_redirect_to_login_page(driver, expected_url):
+    with allure.step('Проверить, что произошел редирект на страницу логина — регистрация успешна'):
+        wait_for_url_contains(driver, expected_url=expected_url)
